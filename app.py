5545d8294c68480195d492f91c9d753ce0ea3567
Wojciech <wojciech.sek17@gmail.com>
2021-05-04 11:42:44 +0200

    3.3 fixup 3.0

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ def login_token(credentials: HTTPBasicCredentials = Depends(security)):
 # 3.3
 @app.get("/welcome_session", status_code=status.HTTP_200_OK)
 def welcome_session(response: Response, request: Request, formatt: Optional[str] = None, session_token: str = Cookie(None)):
-    if session_token is None or session_token == app.session_token:
+    if session_token is not None and session_token == app.session_token:
         if formatt == "json":
             response.headers["content-type"] = "json"
             return {"message": "Welcome!"}
@@ -183,7 +183,7 @@ def welcome_session(response: Response, request: Request, formatt: Optional[str]
 
 @app.get("/welcome_token", status_code=status.HTTP_200_OK)
 def welcome_token(response: Response, request: Request, token: Optional[str] = None, formatt: Optional[str] = None):
-    if token is None or token == app.token:
+    if token is not None and token == app.token:
         if formatt == "json":
             response.headers["content-type"] = "json"
             return {"message": "Welcome!"}
