e5bccb81455f75f548a2655f0143da0fa14c786c
Wojciech <wojciech.sek17@gmail.com>
2021-05-07 19:35:38 +0200

    First attempt of list 4.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import random
+import sqlite3
 import string
 from hashlib import sha512
 from typing import Optional
@@ -9,7 +10,6 @@ from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from datetime import date, timedelta
 from secrets import compare_digest
-
 
 app = FastAPI(
     title="Python Level Up 2021",
@@ -125,7 +125,7 @@ def patient(id: int, response: Response):
 
 # 3.1
 @app.get("/hello")
-def hello_date(response: Response,request: Request):
+def hello_date(response: Response, request: Request):
     today = date.today().strftime("%Y-%m-%d")
     response.headers["content-type"] = "html"
     return templates.TemplateResponse(
@@ -235,3 +235,158 @@ def logged_out(format: str = ""):
         return HTMLResponse(content="<h1>Logged out!</h1>", status_code=200)
     else:
         return PlainTextResponse(content="Logged out!", status_code=200)
+
+
+# 4.1
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect("northwind.db")
+    app.db_connection.text_factory = lambda b: b.decode(errors="ignore")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
+
+
+@app.get("/categories", status_code=status.HTTP_200_OK)
+async def categories():
+    categories = app.db_connection.execute("SELECT CategoryID, CategoryName "
+                                           "FROM Categories "
+                                           "ORDER BY CategoryID").fetchall()
+    return {"categories": [{"id": category[0], "name": category[1]} for category in categories]}
+
+
+@app.get("/customers", status_code=status.HTTP_200_OK)
+async def customers():
+    customers = app.db_connection.execute("SELECT CustomerID, CompanyName, Address, PostalCode, City, Country "
+                                          "FROM Customers ").fetchall()
+    return {"customers": [{"id": customer[0],
+                           "name": customer[1],
+                           "full_address": str(customer[2]) + ' ' +
+                                           str(customer[3]) + ' ' +
+                                           str(customer[4]) + ' ' +
+                                           str(customer[5])} for customer in customers]}
+
+
+# 4.2
+@app.get("/products/{id}")
+async def products(id: int):
+    app.db_connection.row_factory = sqlite3.Row
+    data = app.db_connection.execute(
+        "SELECT ProductID, ProductName FROM Products  WHERE ProductID = :id",
+        {'id': id}).fetchone()
+
+    if not data:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return {"id": data[0], "name": data[1]}
+
+
+# 4.3
+@app.get("/employees")
+async def employees(limit: int, offset: int, order: str = 'EmployeeID'):
+    if order not in ['first_name', 'last_name', 'city', 'EmployeeID']:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+    app.db_connection.row_factory = sqlite3.Row
+
+    data = app.db_connection.execute(f'''
+                    SELECT EmployeeID, LastName, FirstName, City 
+                    FROM Employees
+                    ORDER BY {order}
+                    LIMIT {limit} OFFSET {offset};
+                    ''').fetchall()
+
+    return {"employees": [{"id": x['EmployeeID'],
+                           "last_name": x['LastName'],
+                           "first_name": x['FirstName'],
+                           "city": x['City']} for x in data]}
+
+
+# 4.4
+@app.get("/products_extended")
+async def products_extended():
+    products = app.db_connection.execute(f'''
+                    SELECT Products.ProductID, Products.ProductName, Categories.CategoryName, Suppliers.CompanyName 
+                    FROM Products
+                    JOIN Suppliers ON Products.SupplierID = Suppliers.SupplierID
+                    JOIN Categories ON Products.CategoryID = Categories.CategoryID
+                    ''').fetchall()
+
+    return {"products_extended": [{"id": x[0],
+                                   "name": x[1],
+                                   "category": x[2],
+                                   "supplier": x[3]} for x in products]}
+
+
+# 4.5
+@app.get("/products/{id}/orders")
+async def products_id_orders(id: int):
+    def total(x):
+        unitPrice = x[2]
+        quantity = x[3]
+        discount = x[4]
+        return float("{:.2f}".format((1 - discount) * unitPrice * quantity))
+
+    orders = app.db_connection.execute(f'''
+                    SELECT Orders.OrderID,
+                           Customers.CompanyName,
+                           [Order Details].UnitPrice,
+                           [Order Details].Quantity,
+                           [Order Details].Discount,
+                           [Order Details].ProductID
+                    FROM Orders
+                    JOIN Customers ON Orders.CustomerID = Customers.CustomerID
+                    JOIN [Order Details] ON Orders.OrderID = [Order Details].OrderID
+                    WHERE [Order Details].ProductID = {id};
+                    ''').fetchall()
+
+    if not orders:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    return {"orders": [{"id": x[0],
+                        "customer": x[1],
+                        "quantity": x[3],
+                        "total_price": total(x)} for x in orders]}
+
+
+# 4.6
+class Category(BaseModel):
+    name: str
+
+
+@app.post("/categories", status_code=status.HTTP_201_CREATED)
+async def categories_add(category: Category):
+    cursor = app.db_connection.execute(
+        f"INSERT INTO Categories (CategoryName) VALUES ('{category.name}')"
+    )
+    app.db_connection.commit()
+    return {
+        "id": cursor.lastrowid,
+        "name": category.name
+    }
+
+
+@app.put("/categories/{id}")
+async def categories_put(category: Category):
+    data = app.db_connection.execute(f"SELECT * FROM Categories WHERE CategoryID = {id}").fetchall()
+    if data:
+        cursor = app.db_connection.execute(f"""UPDATE Categories 
+                                               SET CategoryName = {category.name}
+                                               WHERE CategoryID = {id}""")
+        app.db_connection.commit()
+        return {
+            "id": id,
+            "name": category.name
+        }
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.delete("/categories/{id}")
+async def categories_delete(id: int):
+    data = app.db_connection.execute(f"SELECT * FROM Categories WHERE CategoryID = {id}").fetchall()
+    if data:
+        cursor = app.db_connection.execute(f"DELETE FROM Categories WHERE CategoryID = {id}")
+        app.db_connection.commit()
+
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
